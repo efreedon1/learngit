b6d0793a716d57ff7e6d6a88d0e455c6229ac242
freedon <freedon@Freedon-dell2>
2017-06-03 20:20:32 +0300

    #First commit

---
+++ Selenium/src/learningSelenium/testingWebdriver.java
@@ -0,0 +1,19 @@
+package learningSelenium;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class testingWebdriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
